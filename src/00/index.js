04c1182d8e8cfec7343a0cb906aa00a255060e27
xari <harry.anderson@sonarsource.com>
2026-03-24 16:01:10 +0100

    Test Agent PR flow 3

--- src/00/index.js
+++ src/00/index.js
@@ -1,17 +1,19 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate";
 
-const elOutput = document.querySelector('#output');
+let test_trigger_quality_gate_failure = undefined;
+
+const elOutput = document.querySelector("#output");
 
 function output(object) {
   elOutput.innerHTML = JSON.stringify(object, null, 2);
 }
 
-console.log('Welcome to the XState workshop!');
+console.log("Welcome to the XState workshop!");
 
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
+  name: "David Khourshid",
+  company: "Microsoft",
+  interests: ["piano", "state machines"],
 };
 
 output(user);
